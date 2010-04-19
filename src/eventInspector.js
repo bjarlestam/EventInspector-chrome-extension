2bee884a3ec888abeddffc5b71db55404ad570fe
bjarlestam <andreas@bjarlestam.com>
2010-04-19 17:53:16 +0200

    started adding support for live events

--- src/eventInspector.js
+++ src/eventInspector.js
@@ -1,13 +1,23 @@
 
-var MARK_NODES_WITH_EVENTS = 'jQuery("*").filter(function(){return(jQuery(this).data("events"));}).each(function(){jQuery(this).addClass("eventFinder_hasEvent");});';
-//function DISPLAY_EVENTS(top, left) { return 'jQuery("#eventFinder_eventList").remove();var eventFinder_events="jQuery "+jQuery().jquery+" events";jQuery(".eventFinder_selected").each(function(){var dEvents=$(this).data("events");if(!dEvents){return;}for(var type in dEvents){jQuery.each(dEvents[type],function(key,handlerObj){eventFinder_events+="&lt;p&gt;&lt;b&gt;"+type+"&lt;/b&gt; - "+(jQuery().jquery>="1.4.2"?handlerObj.handler:handlerObj)+"&lt;/p&gt;";});}});jQuery("&lt;div&gt;"+eventFinder_events+"&lt;/div&gt;").attr("id","eventFinder_eventList").css({background: "#CCE1E3",border: "3px solid #2E6E75", color: "#000", "font-size":"15px", "text-align":"left", "z-index":"2000",position:"absolute", top:"'+top+'px", left:"'+left+'px", padding:"40px", width:"700px","-webkit-border-radius":"20px","-moz-border-radius":"20px","border-radius":"20px"}).appendTo("body");jQuery("&lt;div&gt; x &lt;/div&gt;").attr("id","eventFinder_close").css({background:"#FFF", width:"30px", "text-align":"center", position:"absolute",right:"10px", top:"10px"}).prependTo("#eventFinder_eventList");jQuery("#eventFinder_close").click(function(){jQuery("#eventFinder_eventList").remove(); return false;})';}
-function DISPLAY_EVENTS(top, left) { return 'jQuery("#eventFinder_popup").remove(); var eventFinder_events="";jQuery(".eventFinder_selected").each(function(){var dEvents=$(this).data("events");if(!dEvents){return;}for(var type in dEvents){jQuery.each(dEvents[type],function(key,handlerObj){eventFinder_events+="<p><b>"+type+"</b> - "+handlerObj.handler+"</p>";})}});jQuery("<div>"+eventFinder_events+"</div>").attr("id","eventFinder_popup").css({top:"'+top+'px", left:"'+left+'px"}).prependTo("body");   jQuery("<div>x</div>").attr("id","eventFinder_close").prependTo("#eventFinder_popup");jQuery("#eventFinder_close").click(function(){jQuery("#eventFinder_popup").remove(); return false;})';}
+var MARK_JQUERY_EVENTS = 'jQuery("*").filter(function(){return(jQuery(this).data("events"));}).each(function(){jQuery(this).addClass("eventFinder_jqEvent");});';
+var MARK_JQUERY_LIVE_EVENTS = 'jQuery.each(jQuery(document).data("events").live, function() {jQuery(this.selector).addClass("eventFinder_jqLive"); });';
 
-var MARKED_ELEMENTS_CSS = '.eventFinder_hasEvent {background-color: #FF00EE;}\n .eventFinder_hasEvent:hover {border: 2px solid #002FFF;}';
-var POPUP_CSS = '#eventFinder_popup {background: #CCE1E3; border: 3px solid #2E6E75; color: #000; font-family:arial; font-size:15px; z-index:2000; position:absolute; top 60px; left:50px; padding:30px; width:700px;-webkit-border-radius:20px;-moz-border-radius:20px;border-radius:20px;} \n #eventFinder_close {background:#FFF; width:30px; text-align:center; position:absolute; right:10px; top:10px; cursor:pointer;}';
+function DISPLAY_JQUERY_EVENTS(top, left) { return 'jQuery("#eventFinder_popup").remove(); var eventFinder_events="";jQuery(".eventFinder_selected").each(function(){var dEvents=$(this).data("events");if(!dEvents){return;}for(var type in dEvents){jQuery.each(dEvents[type],function(key,handlerObj){eventFinder_events+="<p><b>"+type+"</b> - "+handlerObj.handler+"</p>";})}});jQuery("<div>"+eventFinder_events+"</div>").attr("id","eventFinder_popup").css({top:"'+top+'px", left:"'+left+'px"}).prependTo("body");   jQuery("<div>x</div>").attr("id","eventFinder_close").prependTo("#eventFinder_popup");jQuery("#eventFinder_close").click(function(){jQuery("#eventFinder_popup").remove(); return false;})';}
 
-var sc = DISPLAY_EVENTS(100, 100);
+function DISPLAY_EVENTS(top, left) { return 'jQuery("#eventFinder_popup").remove();    jQuery("<div>"+eventFinder_events+"</div>").attr("id","eventFinder_popup").css({top:"'+top+'px", left:"'+left+'px"}).prependTo("body");   jQuery("<div>x</div>").attr("id","eventFinder_close").prependTo("#eventFinder_popup");jQuery("#eventFinder_close").click(function(){jQuery("#eventFinder_popup").remove(); return false;})';}
 
+var DISPLAY_JQUERY_LIVE_EVENTS = 'var eventFinder_events="hello hello"';
+
+var MARKED_ELEMENTS_CSS =   '.eventFinder_jqEvent {background-color: #FF00EE;} ' + 
+                            '.eventFinder_jqEvent:hover {border: 2px solid #002FFF;} ' +
+                            '.eventFinder_jqLive {background-color: gold;}' +
+                            '.eventFinder_jqLive:hover {border: 2px solid #002FFF;} ';
+                            
+                            
+var POPUP_CSS = '#eventFinder_popup {background: #CCE1E3; border: 3px solid #2E6E75; color: #000; font-family:arial; font-size:15px; z-index:3000; position:absolute; padding:30px; -webkit-border-radius:20px;-moz-border-radius:20px;border-radius:20px;} ' +
+                '#eventFinder_close {background:#FFF; width:30px; text-align:center; position:absolute; right:10px; top:10px; cursor:pointer;}';
+
+var IS_MICROSOFT = navigator.userAgent.toLowerCase().indexOf('') !== -1;
 
 function escapeHtml(str) {
     return str.replace(/</g, '&lt;').replace(/>/g,'&gt;');
@@ -28,13 +38,23 @@ function injectCss(css) {
 injectCss(MARKED_ELEMENTS_CSS);
 injectCss(POPUP_CSS);
 
-injectScript(MARK_NODES_WITH_EVENTS, 'eventFinder_mark');
+injectScript(MARK_JQUERY_EVENTS, 'eventFinder_jqScript');
+injectScript(MARK_JQUERY_LIVE_EVENTS, 'eventFinder_jqLiveScript');
 
 
-jQuery('.eventFinder_hasEvent').click(function(event) {
-    $('.eventFinder_selected').removeClass('eventFinder_selected');
-    $(this).addClass('eventFinder_selected');
-    $('#eventFinder_display').remove();
-    injectScript(DISPLAY_EVENTS(event.pageY, 100), 'eventFinder_display');
+jQuery('.eventFinder_jqEvent').click(function(event) {
+    jQuery('.eventFinder_selected').removeClass('eventFinder_selected');
+    jQuery(this).addClass('eventFinder_selected');
+    jQuery('#eventFinder_displayScript').remove();
+    injectScript(DISPLAY_JQUERY_EVENTS(event.pageY, 100), 'eventFinder_displayScript');
+    return false;
+});
+
+jQuery('.eventFinder_jqLive').click(function(event) {
+    jQuery('.eventFinder_selected').removeClass('eventFinder_selected');
+    jQuery(this).addClass('eventFinder_selected');
+    jQuery('#eventFinder_displayScript').remove();
+    injectScript(DISPLAY_JQUERY_LIVE_EVENTS, 'eventFinder_live');
+    injectScript(DISPLAY_EVENTS(event.pageY, 100), 'eventFinder_displayScript');
     return false;
 });
--- src/liveEvents.js
+++ src/liveEvents.js
@@ -13,9 +13,6 @@
 	function markLiveEvents() {
 		jQuery.each(jQuery(document).data("events").live, function() {
 		    jQuery(this.selector).addClass("eventFinder_hasEvent");
-			if(element.is(this.selector)) {
-				result += this.type + '.' + this.origType + ' - ' + this.origHandler + '\n'; 
-			}
 		});
 	}
 	
