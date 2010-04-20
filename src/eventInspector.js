12945244c7afa955e335ed38ca5ac5766be89efb
bjarlestam <andreas@bjarlestam.com>
2010-04-20 21:41:36 +0200

    added fix for windows chrome version

--- src/eventInspector.js
+++ src/eventInspector.js
@@ -8,16 +8,16 @@ function DISPLAY_EVENTS(top, left) { return 'jQuery("#eventFinder_popup").remove
 
 var DISPLAY_JQUERY_LIVE_EVENTS = 'var eventFinder_events="";  jQuery.each(jQuery(document).data("events").live, function() {if(jQuery(".eventFinder_selected").is(this.selector)) { eventFinder_events += "<p><b>"+this.type + "." + this.origType + "</b> - " + this.origHandler+"</p>"; }});';
 
-var MARKED_ELEMENTS_CSS =   '.eventFinder_jqEvent {background-color: #FF00EE;} ' + 
+var MARKED_ELEMENTS_CSS =   '.eventFinder_jqEvent {background: #FF00EE;} ' + 
                             '.eventFinder_jqEvent:hover {border: 2px solid #002FFF;} ' +
-                            '.eventFinder_jqLive {background-color: gold;}' +
+                            '.eventFinder_jqLive {background: gold;} ' +
                             '.eventFinder_jqLive:hover {border: 2px solid #002FFF;} ';
                             
                             
 var POPUP_CSS = '#eventFinder_popup {background: #CCE1E3; border: 3px solid #2E6E75; color: #000; font-family:arial; font-size:15px; z-index:3000; position:absolute; padding:30px; -webkit-border-radius:20px;-moz-border-radius:20px;border-radius:20px;} ' +
                 '#eventFinder_close {background:#FFF; width:30px; text-align:center; position:absolute; right:10px; top:10px; cursor:pointer;}';
 
-var IS_MICROSOFT = navigator.userAgent.toLowerCase().indexOf('') !== -1;
+var IS_WIN = navigator.userAgent.toLowerCase().indexOf('windows') !== -1;
 
 function escapeHtml(str) {
     return str.replace(/</g, '&lt;').replace(/>/g,'&gt;');
@@ -26,6 +26,7 @@ function escapeHtml(str) {
 
 function injectScript(scr ,id) {
     var s = jQuery('<script id="' + id + '"></script>');
+    if(IS_WIN) {scr = escapeHtml(scr);}
     s.html(scr);
     s.appendTo('head');
 }
